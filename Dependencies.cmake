7ac70bb28420fe0a3672172c68bfa46e2c73a111
Marek Białoruski <mar.bialoruski@gmail.com>
2023-09-19 12:21:26 +0000

    feat: basic imgui example with SDL2 and OpenGL3

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -9,27 +9,47 @@ function(project_setup_dependencies)
   # already been provided to us by a parent project
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+    cpmaddpackage("gh:fmtlib/fmt#10.1.1")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
     cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
+      GITHUB_REPOSITORY gabime/spdlog
+      VERSION 1.12.0
       OPTIONS
       "SPDLOG_FMT_EXTERNAL ON")
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    cpmaddpackage("gh:catchorg/Catch2#v3.4.0")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+    cpmaddpackage("gh:CLIUtils/CLI11#v2.3.2")
+  endif()
+
+  if(NOT TARGET SDL2::SDL2)
+    cpmaddpackage("gh:libsdl-org/SDL#release-2.28.3")
+  endif()
+
+  if(NOT TARGET imgui)
+    cpmaddpackage("gh:ocornut/imgui#v1.89.9")
+
+    add_library(imgui STATIC
+    ${imgui_SOURCE_DIR}/imgui.cpp
+    ${imgui_SOURCE_DIR}/imgui_draw.cpp
+    ${imgui_SOURCE_DIR}/imgui_demo.cpp
+    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl2.cpp
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+    )
+
+    target_include_directories(imgui PUBLIC ${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/backends)
+    target_compile_definitions(imgui PUBLIC -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS)
+
+    target_link_system_libraries(imgui PRIVATE SDL2::SDL2)
   endif()
 
 endfunction()
