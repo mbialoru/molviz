b74b3f32540aa2f933e10c03e3c49aeaf9b8116b
Marek Białoruski <mar.bialoruski@gmail.com>
2023-10-10 09:38:44 +0000

    docker X11 forwarding for fedora fix

--- src/molviz/main.cpp
+++ src/molviz/main.cpp
@@ -63,8 +63,8 @@ int main(int argc, char **argv)
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
   SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-  SDL_Window *window = SDL_CreateWindow(
-    "Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, window_flags);
+  SDL_Window *window =
+    SDL_CreateWindow("Molviz", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, window_flags);
   SDL_GLContext gl_context = SDL_GL_CreateContext(window);
   glewInit();
   SDL_GL_MakeCurrent(window, gl_context);
@@ -88,6 +88,8 @@ int main(int argc, char **argv)
 
   // Our state
   ImVec4 clear_color = ImVec4(1.00F, 1.00F, 1.00F, 1.00F);
+
+  spdlog::debug("imgui configured");
 
   // Vertices coordinates
   Vertex model_vertices_raw[] = {
