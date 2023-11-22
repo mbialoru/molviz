b510e760093ffb1c6c538e6a36c7df5631587f2d
Marek Białoruski <mar.bialoruski@gmail.com>
2023-11-22 10:49:15 +0000

    refactor: move semantics for mesh

--- test/tests.cpp
+++ test/tests.cpp
@@ -123,4 +123,24 @@ TEST_CASE("Shader valid creation and cleanup", "[Shader]")
   cleanup_dummy_opengl_context(p_window, context);
 }
 
-TEST_CASE("Mesh valid creation and cleanup", "[Mesh]") { FAIL("Not yet implemented"); }
+TEST_CASE("Mesh valid creation and cleanup", "[Mesh]")
+{
+  using namespace Molviz::gfx;
+
+  std::vector<Vertex> vertices{ Vertex{ glm::vec3(-0.1f, -0.1f, 0.1f) },
+    Vertex{ glm::vec3(-0.1f, -0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, -0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, -0.1f, 0.1f) },
+    Vertex{ glm::vec3(-0.1f, 0.1f, 0.1f) },
+    Vertex{ glm::vec3(-0.1f, 0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, 0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, 0.1f, 0.1f) } };
+
+  std::vector<GLuint> indices{
+    0, 1, 2, 0, 2, 3, 0, 4, 7, 0, 7, 3, 3, 7, 6, 3, 6, 2, 2, 6, 5, 2, 5, 1, 1, 5, 4, 1, 4, 0, 4, 5, 6, 4, 6, 7
+  };
+
+  auto [p_window, context]{ create_dummy_opengl_context() };
+
+  Mesh mesh{ vertices, indices };
+}
