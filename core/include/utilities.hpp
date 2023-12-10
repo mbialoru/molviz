adbd9fb68275d788aa4cdd285fb9fd477bfc13cd
Marek Białoruski <mar.bialoruski@gmail.com>
2023-12-10 21:57:50 +0000

    [WIP] extracting gltf parser from model

--- core/include/utilities.hpp
+++ core/include/utilities.hpp
@@ -17,55 +17,32 @@ std::string file_contents_to_string(const char *tp_file);
 std::string file_contents_to_string(const std::string &tr_file);
 std::string file_contents_to_string(const std::filesystem::path &tr_file);
 
-template<typename T> std::vector<glm::vec<2, T, glm::defaultp>> group_data_vec2(const std::vector<T> &tr_data)
+template<typename T> std::vector<T> convert_bytes(const std::vector<uint8_t> &tr_data)
 {
-  if (tr_data.size() % 2 != 0) { throw InvalidArgumentLogged(fmt::format("vector length not divisible by {}", 2)); }
-
-  std::vector<glm::vec<2, T, glm::defaultp>> grouped;
-
-  for (std::size_t i{ 0 }; i < tr_data.size() / 2; i += 2) { grouped.emplace_back(tr_data[i + 0], tr_data[i + 1]); }
-
-  return grouped;
-};
-
-template<typename T> std::vector<glm::vec<3, T, glm::defaultp>> group_data_vec3(const std::vector<T> &tr_data)
-{
-  if (tr_data.size() % 3 != 0) { throw InvalidArgumentLogged(fmt::format("vector length not divisible by {}", 3)); }
-
-  std::vector<glm::vec<3, T, glm::defaultp>> grouped;
-
-  for (std::size_t i{ 0 }; i < tr_data.size() / 3; i += 3) {
-    grouped.emplace_back(tr_data[i + 0], tr_data[i + 1], tr_data[i + 2]);
+  if (tr_data.size() % sizeof(T) != 0) {
+    throw InvalidArgumentLogged(fmt::format("vector length not divisible by {}", sizeof(T)));
   }
-  return grouped;
-};
 
-template<typename T> std::vector<glm::vec<4, T, glm::defaultp>> group_data_vec4(const std::vector<T> &tr_data)
-{
-  if (tr_data.size() % 4 != 0) { throw InvalidArgumentLogged(fmt::format("vector length not divisible by {}", 4)); }
+  std::vector<T> converted;
 
-  std::vector<glm::vec<4, T, glm::defaultp>> grouped;
-
-  for (std::size_t i{ 0 }; i < tr_data.size() / 4; i += 4) {
-    grouped.emplace_back(tr_data[i + 0], tr_data[i + 1], tr_data[i + 2], tr_data[i + 3]);
+  for (std::size_t i{ 0 }; i < tr_data.size() / sizeof(T); i += sizeof(T)) {
+    std::array<uint8_t, sizeof(T)> bytes;
+    for (std::size_t i{ 0 }; i < sizeof(T); ++i) { bytes[i] = tr_data[i]; }
+    T value;
+    std::memmove(&value, bytes.data(), sizeof(T));
+    converted.push_back(value);
   }
-  return grouped;
-};
-
-template<typename T> std::size_t get_vertex_stride(std::string t_type)
-{
-  if (t_type == "SCALAR")
-    return 1;
-  else if (t_type == "VEC2")
-    return sizeof(glm::vec<2, T, glm::qualifier::defaultp>) / sizeof(T);
-  else if (t_type == "VEC3")
-    return sizeof(glm::vec<3, T, glm::qualifier::defaultp>) / sizeof(T);
-  else if (t_type == "VEC4")
-    return sizeof(glm::vec<4, T, glm::qualifier::defaultp>) / sizeof(T);
-  else {
-    throw InvalidArgumentLogged(fmt::format("type is invalid {} given", t_type));
-  }
+  return converted;
 }
+
+// unary functor
+struct map_byte_to_float
+{
+  float operator()(const uint8_t t_input) const
+  {
+    return { static_cast<float>(t_input) / static_cast<float>(std::numeric_limits<uint8_t>::max()) };
+  }
+};
 
 }// namespace Molviz
 
--- test/test_gfx.cpp
+++ test/test_gfx.cpp
@@ -125,14 +125,14 @@ TEST_CASE("Mesh valid creation and cleanup", "[Mesh]")
 {
   using namespace Molviz::gfx;
 
-  std::vector<Vertex> vertices{ Vertex{ glm::vec3(-0.1F, -0.1F, 0.1F) },
-    Vertex{ glm::vec3(-0.1F, -0.1F, -0.1F) },
-    Vertex{ glm::vec3(0.1F, -0.1F, -0.1F) },
-    Vertex{ glm::vec3(0.1F, -0.1F, 0.1F) },
-    Vertex{ glm::vec3(-0.1F, 0.1F, 0.1F) },
-    Vertex{ glm::vec3(-0.1F, 0.1F, -0.1F) },
-    Vertex{ glm::vec3(0.1F, 0.1F, -0.1F) },
-    Vertex{ glm::vec3(0.1F, 0.1F, 0.1F) } };
+  std::vector<Vertex> vertices{ Vertex{ glm::vec3(-0.1f, -0.1f, 0.1f) },
+    Vertex{ glm::vec3(-0.1f, -0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, -0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, -0.1f, 0.1f) },
+    Vertex{ glm::vec3(-0.1f, 0.1f, 0.1f) },
+    Vertex{ glm::vec3(-0.1f, 0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, 0.1f, -0.1f) },
+    Vertex{ glm::vec3(0.1f, 0.1f, 0.1f) } };
 
   std::vector<GLuint> indices{
     0, 1, 2, 0, 2, 3, 0, 4, 7, 0, 7, 3, 3, 7, 6, 3, 6, 2, 2, 6, 5, 2, 5, 1, 1, 5, 4, 1, 4, 0, 4, 5, 6, 4, 6, 7
