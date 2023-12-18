752bae4b99cd8c0906390e0c795720500d6d725d
Marek Białoruski <mar.bialoruski@gmail.com>
2023-12-18 21:40:51 +0000

    test utils

--- test/include/test_gfx_utilities.hpp
+++ test/include/test_gfx_utilities.hpp
@@ -29,7 +29,30 @@ std::pair<SDL_Window *, SDL_GLContext> create_dummy_opengl_context()
     spdlog::error("GLEW init failed! error: {}", error_message);
     FAIL();
   }
+  return { p_window, context };
+}
 
+std::pair<SDL_Window *, SDL_GLContext> create_real_opengl_context()
+{
+  // init SDL
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    spdlog::error("SDL initialization error: {}", SDL_GetError());
+    FAIL();
+  }
+
+  // create real visible SDL window for OpenGL context
+  SDL_WindowFlags window_flags{ static_cast<SDL_WindowFlags>(SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN) };
+  SDL_Window *p_window{ SDL_CreateWindow(
+    nullptr, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, window_flags) };
+  SDL_GLContext context{ SDL_GL_CreateContext(p_window) };
+
+  // init GLEW after OpenGL context - Missing GL version error otherwise
+  GLenum status{ glewInit() };
+  if (status != GLEW_OK) {
+    std::string error_message{ reinterpret_cast<const char *>(glewGetErrorString(status)) };
+    spdlog::error("GLEW init failed! error: {}", error_message);
+    FAIL();
+  }
   return { p_window, context };
 }
 
