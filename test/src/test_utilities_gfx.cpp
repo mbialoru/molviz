a360ebe31508d8239e809aea7cd02ebf421995c1
Marek Białoruski <mar.bialoruski@gmail.com>
2023-12-20 13:47:53 +0000

    feat: more responsive controls

--- test/src/test_utilities_gfx.cpp
+++ test/src/test_utilities_gfx.cpp
@@ -1,12 +1,4 @@
-#ifndef TEST_GFX_UTILITIES_HPP
-#define TEST_GFX_UTILITIES_HPP
-
-#pragma once
-
-#include <GL/glew.h>
-#include <SDL.h>
-#include <catch2/catch_test_macros.hpp>
-#include <spdlog/spdlog.h>
+#include "test_utilities_gfx.hpp"
 
 // create dummy zero-size window for valid opengl context
 std::pair<SDL_Window *, SDL_GLContext> create_dummy_opengl_context()
@@ -33,6 +25,31 @@ std::pair<SDL_Window *, SDL_GLContext> create_dummy_opengl_context()
   return { p_window, context };
 }
 
+// create visible window with opengl context
+std::pair<SDL_Window *, SDL_GLContext> create_real_opengl_context()
+{
+  // init SDL
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    spdlog::error("SDL initialization error: {}", SDL_GetError());
+    FAIL();
+  }
+
+  // create real visible SDL window for OpenGL context
+  SDL_WindowFlags window_flags{ static_cast<SDL_WindowFlags>(SDL_WINDOW_OPENGL) };
+  SDL_Window *p_window{ SDL_CreateWindow(
+    nullptr, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, window_flags) };
+  SDL_GLContext context{ SDL_GL_CreateContext(p_window) };
+
+  // init GLEW after OpenGL context - Missing GL version error otherwise
+  GLenum status{ glewInit() };
+  if (status != GLEW_OK) {
+    std::string error_message{ reinterpret_cast<const char *>(glewGetErrorString(status)) };
+    spdlog::error("GLEW init failed! error: {}", error_message);
+    FAIL();
+  }
+  return { p_window, context };
+}
+
 // delete context and destroy window
 void cleanup_opengl_context(SDL_Window *tp_window, SDL_GLContext t_context)
 {
@@ -40,5 +57,3 @@ void cleanup_opengl_context(SDL_Window *tp_window, SDL_GLContext t_context)
   SDL_DestroyWindow(tp_window);
   SDL_Quit();
 }
-
-#endif// TEST_GFX_UTILITIES_HPP
