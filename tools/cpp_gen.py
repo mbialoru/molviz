9cb1d2bae2e94b2bb01adfa19ba9efdaad5a1e50
Marek Białoruski <mar.bialoruski@gmail.com>
2024-01-03 11:05:33 +0000

    feat: cpp_gen update

--- tools/cpp_gen.py
+++ tools/cpp_gen.py
@@ -17,24 +17,24 @@ class convert_to_pathlib(argparse.Action):
 
 def setup_argv() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", type=str)
     parser.add_argument("cpp", nargs="?", default=Path("."), action=convert_to_pathlib)
     parser.add_argument("hpp", nargs="?", default=Path("."), action=convert_to_pathlib)
     parser.add_argument("-include", nargs="?", action=convert_to_pathlib, default=Path("."))
     parser.add_argument("-ipp", help="generate ipp file", default=False, action="store_true")
     parser.add_argument("-ns", "--namespace", type=str, action="store")
     parser.add_argument("-c", "--class", type=str, action="store")
-    parser.add_argument("--overwrite", help="allow overwrite", default=False, action="store_true")
+    parser.add_argument("-ow", "--overwrite", help="allow overwrite", default=False, action="store_true")
+    parser.add_argument("filename", type=str, nargs="?", default="")
     return parser.parse_args()
 
 
 def check_file_exists(file_path: Path, overwrite: bool):
     if file_path.exists() and file_path.is_file():
         if overwrite:
-            LOGGER.warning(f"file exists, overwriting")
+            LOGGER.warning("file exists, overwriting")
             return False
         else:
-            LOGGER.warning(f"file exists, skipping")
+            LOGGER.warning("file exists, skipping")
             return True
     else:
         return False
@@ -50,12 +50,15 @@ def setup_logger(debug: bool = False) -> None:
 
 
 def main(argv: argparse.Namespace):
+    if not argv.filename and getattr(argv, "class"):
+        setattr(argv, "filename", getattr(argv, "class").lower())
+
     filename_lower = argv.filename.replace(" ", "").lower()
     filename_upper = argv.filename.replace(" ", "").upper()
 
     # write implementation
     if argv.ipp:
-        ipp_path = getattr(argv, "hpp") / f"{filename_lower}.ipp"
+        ipp_path = argv.hpp / f"{filename_lower}.ipp"
 
         LOGGER.info(f"writing implementation {ipp_path}")
 
@@ -64,7 +67,7 @@ def main(argv: argparse.Namespace):
             ipp_path.touch()
 
     # write header
-    hpp_path = getattr(argv, "hpp") / f"{filename_lower}.hpp"
+    hpp_path = argv.hpp / f"{filename_lower}.hpp"
 
     LOGGER.info(f"writing header {hpp_path}")
 
@@ -74,7 +77,7 @@ def main(argv: argparse.Namespace):
             hpp_file.write(f"#define {filename_upper}_HPP\n\n")
             hpp_file.write("#pragma once\n\n")
             hpp_file.write("// Includes here\n")
-            if namespace := getattr(argv, "namespace"):
+            if namespace := argv.namespace:
                 hpp_file.write(f"\nnamespace {namespace} {{\n\n")
             if name := getattr(argv, "class"):
                 hpp_file.write(f"class {name}\n{{\n")
@@ -85,13 +88,13 @@ def main(argv: argparse.Namespace):
                 hpp_file.write("};")
             if argv.ipp:
                 hpp_file.write(f"\n\n#include \"{ipp_path.relative_to(argv.include)}\"")
-            if namespace := getattr(argv, "namespace"):
+            if namespace := argv.namespace:
                 hpp_file.write(f"\n\n}}// namespace {namespace}")
             hpp_file.write("\n\n")
             hpp_file.write(f"#endif// {filename_upper}_HPP")
 
     # write source
-    cpp_path = getattr(argv, "cpp") / f"{filename_lower}.cpp"
+    cpp_path = argv.cpp / f"{filename_lower}.cpp"
     LOGGER.info(f"writing source {cpp_path}")
 
     if not check_file_exists(cpp_path, argv.overwrite):
@@ -100,7 +103,7 @@ def main(argv: argparse.Namespace):
                 cpp_file.write(f'#include "{hpp_path.relative_to(argv.include)}"\n')
             except ValueError:
                 cpp_file.write(f'#include "{hpp_path}"\n')
-            if namespace := getattr(argv, "namespace"):
+            if namespace := argv.namespace:
                 cpp_file.write(f"\nusing namespace {namespace};\n")
             if name := getattr(argv, "class"):
                 cpp_file.write(f"\n{name}::{name}() {{}};")
