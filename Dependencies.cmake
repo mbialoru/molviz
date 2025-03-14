9db647c87ca783c549d50a91f3efcf4bbcf799d9
Marek Białoruski <mar.bialoruski@gmail.com>
2025-03-14 21:27:49 +0000

    build: using simpler format.cmake cpm declaration

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -61,23 +61,7 @@ function(project_setup_dependencies)
   endif()
 
   if(NOT TARGET Format)
-    cpmaddpackage(
-      NAME
-      Format.cmake
-      VERSION
-      1.8.3
-      GITHUB_REPOSITORY
-      TheLartians/Format.cmake
-      OPTIONS
-      # set to yes skip cmake formatting
-      "FORMAT_SKIP_CMAKE NO"
-      # set to yes skip clang formatting
-      "FORMAT_SKIP_CLANG NO"
-      # path to exclude (optional, supports regular expressions)
-      "CMAKE_FORMAT_EXCLUDE out"
-      # extra arguments for cmake_format (optional)
-      # "CMAKE_FORMAT_EXTRA_ARGS -c clang-format.yaml"
-    )
+    cpmaddpackage("gh:TheLartians/Format.cmake@1.8.3")
   endif()
 
 endfunction()
