1b4341ad1a32832a243eb517551cb23b301e9bd8
Marek Białoruski <mar.bialoruski@gmail.com>
2025-03-14 22:25:07 +0000

    feat: event base class

---
+++ core/include/io/event.hpp
@@ -0,0 +1,32 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+#pragma once
+
+#include <cstdint>
+
+namespace mve::io {
+
+enum class EventType : std::uint8_t {
+  KEY_PRESSED,
+  KEY_RELEASED,
+  MOUSE_MOVED,
+  MOUSE_BUTTON_PRESSED,
+  MOUSE_BUTTON_RELEASED
+};
+
+class Event
+{
+public:
+  explicit Event(EventType t_type);
+  virtual ~Event() = default;
+
+  [[nodiscard]] EventType virtual get_type() const = 0;
+
+protected:
+  EventType m_type;
+};
+
+}// namespace mve::io
+
+#endif// EVENT_HPP
---
+++ core/src/io/event.cpp
@@ -0,0 +1,5 @@
+#include "io/event.hpp"
+
+using namespace mve::io;
+
+Event::Event(EventType t_type) : m_type(t_type) {};
