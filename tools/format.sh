e1265caf916c68af2805e3a141a430718b3a32dc
Marek Białoruski <mar.bialoruski@gmail.com>
2025-03-14 20:45:48 +0000

    chore: removing formatting script
    fix-format target provides this functionality

--- tools/format.sh
+++
@@ -1,7 +0,0 @@
-# apply cmake-format
-find /workspaces/molviz -type f -name "*.cmake" -not -path
-"/workspaces/molviz/out/*" -exec cmake-format -i {} \;
-find /workspaces/molviz -type f -name "CMakeLists.txt" -exec cmake-format -i {} \;
-
-# apply clang-format
-find /workspaces/molviz -type f -name "*.*pp" -not -path "/workspaces/molviz/out/*" -exec clang-format -i {} \;
