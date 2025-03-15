033f8793a573bdb8225895e75280cc7de40afcab
Marek Białoruski <mar.bialoruski@gmail.com>
2025-03-15 10:14:25 +0000

    fix: added missing getters

--- core/include/io/mouseevent.hpp
+++ core/include/io/mouseevent.hpp
@@ -13,6 +13,9 @@ public:
   MouseEvent(int t_x, int t_y);
   ~MouseEvent() override = default;
 
+  [[nodiscard]] int get_x() const;
+  [[nodiscard]] int get_y() const;
+
 private:
   int m_x;
   int m_y;
--- core/src/io/mouseevent.cpp
+++ core/src/io/mouseevent.cpp
@@ -4,3 +4,7 @@
 using namespace mve::io;
 
 MouseEvent::MouseEvent(int t_x, int t_y) : Event(EventType::MOUSE_MOVE), m_x(t_x), m_y(t_y) {};
+
+int MouseEvent::get_x() const { return m_x; }
+
+int MouseEvent::get_y() const { return m_y; }
