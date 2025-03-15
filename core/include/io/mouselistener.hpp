57f5676d13350eb267d5f5b051ba3dc2e1765103
Marek Białoruski <mar.bialoruski@gmail.com>
2025-03-15 10:37:51 +0000

    feat: mouse event listener
    supporting two types of events

--- core/include/io/mouselistener.hpp
+++ core/include/io/mouselistener.hpp
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "eventlistener.hpp"
+#include "mouseevent.hpp"
+#include "mousekeyevent.hpp"
 
 namespace mve::io {
 
@@ -13,6 +15,8 @@ public:
   ~MouseListener() override = default;
 
   void on_event(const Event &tr_event) override;
+  static void on_event(const MouseEvent &tr_event);
+  static void on_event(const MouseKeyEvent &tr_event);
 };
 
 }// namespace mve::io
--- core/src/io/mouselistener.cpp
+++ core/src/io/mouselistener.cpp
@@ -1,34 +1,70 @@
 #include "io/mouselistener.hpp"
 #include "exceptions.hpp"
+#include <spdlog/spdlog.h>
+#include <typeinfo>
 
 using namespace mve::io;
 
 void MouseListener::on_event(const Event &tr_event)
 {
-  // TODO: conditional cast to infer if its mouse movement or mouse click
-  // const KeyEvent &key_event{ dynamic_cast<const KeyEvent &>(tr_event) };
+  try {
+    const MouseEvent &r_mouse_event{ dynamic_cast<const MouseEvent &>(tr_event) };
+    on_event(r_mouse_event);
+  } catch (const std::bad_cast &e) {
+    spdlog::debug("not a mouse event");
+  }
 
-  // std::stringstream sstream;
+  try {
+    const MouseKeyEvent &r_mouse_key_event{ dynamic_cast<const MouseKeyEvent &>(tr_event) };
+    on_event(r_mouse_key_event);
+  } catch (const std::bad_cast &e) {
+    spdlog::debug("not a mouse key event");
+  }
 
-  // switch (key_event.get_type()) {
-  // case EventType::KEY_PRESS:
-  //   sstream << "Key Pressed ";
-  //   break;
-  // case EventType::KEY_RELEASE:
-  //   sstream << "Key Released ";
-  //   break;
-  // case EventType::KEY_CONTINUOUS:
-  //   sstream << "Key Continuous ";
-  //   break;
-  // case EventType::KEY_DOUBLE:
-  //   sstream << "Key Double ";
-  //   break;
-  // default:
-  //   throw InvalidEvent();
-  //   break;
-  // }
+  throw InvalidEvent();
+}
 
-  // sstream << key_event.get_code();
+void MouseListener::on_event(const MouseEvent &tr_event)
+{
+  std::stringstream sstream;
 
-  // spdlog::info(sstream.str());
+  switch (tr_event.get_type()) {
+  case EventType::MOUSE_MOVE:
+    sstream << "Mouse Moved ";
+    break;
+  default:
+    throw InvalidEvent();
+    break;
+  }
+
+  sstream << tr_event.get_x() << tr_event.get_y();
+
+  spdlog::info(sstream.str());
+}
+
+void MouseListener::on_event(const MouseKeyEvent &tr_event)
+{
+  std::stringstream sstream;
+
+  switch (tr_event.get_type()) {
+  case EventType::KEY_PRESS:
+    sstream << "Key Pressed ";
+    break;
+  case EventType::KEY_RELEASE:
+    sstream << "Key Released ";
+    break;
+  case EventType::KEY_CONTINUOUS:
+    sstream << "Key Continuous ";
+    break;
+  case EventType::KEY_DOUBLE:
+    sstream << "Key Double ";
+    break;
+  default:
+    throw InvalidEvent();
+    break;
+  }
+
+  sstream << static_cast<int>(tr_event.get_key());
+
+  spdlog::info(sstream.str());
 }
