2882cb3b5b2d2d0ea4b12807fb37c3d3af55db56
Marek Białoruski <mar.bialoruski@gmail.com>
2025-03-15 22:36:49 +0000

    test: first event based input tests

--- test/src/unit/test_io.cpp
+++ test/src/unit/test_io.cpp
@@ -1,31 +1,30 @@
 #include <catch2/catch_test_macros.hpp>
-#include <chrono>
-#include <thread>
+#include <random>
 
+#include "io/event.hpp"
+#include "io/eventdispatcher.hpp"
+#include "io/keyboardlistener.hpp"
+#include "io/keyevent.hpp"
 #include "test_utilities.hpp"
-#include "test_utilities_gfx.hpp"
-
-#include "io/inputhandler.hpp"
 
 CATCH_REGISTER_LISTENER(trace_log_level)
 
-TEST_CASE("InputHandler valid creation and cleanup", "[InputHandler]")
+TEST_CASE("KeyboardListener key press", "[KeyboardListener]")
 {
   using namespace mve::io;
 
-  REQUIRE_NOTHROW([&]() { InputHandler input_handler; }());
-}
+  std::random_device device;
+  std::mt19937 generator(device());
+  std::uniform_int_distribution<std::mt19937::result_type> distribution(0, 255);
 
-TEST_CASE("InputHandler capturing input", "[InputHandler, Manual]")
-{
-  using namespace mve::io;
-  using namespace std::chrono_literals;
+  REQUIRE_NOTHROW([&]() {
+    auto event_dispatcher{ EventDispatcher() };
+    auto keyboard_listener{ KeyboardListener() };
 
-  auto [p_window, context]{ create_real_opengl_context() };
+    event_dispatcher.add_listener(&keyboard_listener);
 
-  InputHandler input_handler;
+    const KeyEvent a_press{ static_cast<int>(distribution(generator)), EventType::KEY_PRESS };
 
-  std::this_thread::sleep_for(15s);
-
-  cleanup_opengl_context(p_window, context);
+    event_dispatcher.dispatch(a_press);
+  }());
 }
